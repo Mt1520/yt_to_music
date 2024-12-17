70c9ea673b3c00d28f83c946af4cb3a0563a8238
Mostafa <mt1520@ic.ac.uk>
2024-12-17 21:36:04 +0000

    debugging: trying with single videos

--- main.py
+++ main.py
@@ -7,18 +7,21 @@ import sys
 def main():
     print("Enter the URL of the video you want to download: ")
     url = input()
-    playlist = Playlist(url)
+    # playlist = Playlist(url)
+    # download_path = "D:/mtrih/Music"
+    # i = 0
+
+    # for video in playlist.videos:
+    #     # video_title = video.title if video.title else "default_title" + i
+    #     # video.register_on_progress_callback(on_progress)
+    #     video.streams.filter(only_audio=True).first().download(output_path=download_path)
+    #     i+=1
+    # print ("Downloaded all videos in the playlist")
+
+    video = YouTube(url)
     download_path = "D:/mtrih/Music"
-    i = 0
-
-    for video in playlist.videos:
-        # video_title = video.title if video.title else "default_title" + i
-        # video.register_on_progress_callback(on_progress)
-        video.streams.filter(only_audio=True).first().download(output_path=download_path)
-        i+=1
-    print ("Downloaded all videos in the playlist")
-
-
+    video.streams.filter(only_audio=True).first().download(output_path=download_path)
+    print("Downloaded the video")
 
 
 main()
